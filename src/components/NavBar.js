10e911436b2e47e65f0edc04111d11dee2b611a0
Bernadette McKnight <sissyhanks@yahoo.com>
2021-11-17 11:53:19 -0500

    style xs breakpoint

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,19 +13,19 @@ function NavBar() {
   return (
   <ThemeProvider theme={theme}>
     <Container>
-      <AppBar>
-        <Box sx={{ display: 'flex', direction: { xs: 'column', sm: 'row' }, justifyContent: 'space-between', flexDirection: { xs: 'column-reverse', sm: 'row' } }}>
-            <Box sx={{ ml: { xs: 0, sm: 5 }, display: 'flex', flexDirection: { xs: 'row', sm: 'column' }, justifyContent: 'space-evenly' }}>
+      <AppBar variant="primary">
+        <Box sx={{ display: 'flex', direction: { xs: 'column', sm: 'column', md: 'row' }, justifyContent: 'space-between', flexDirection: { xs: 'column', sm: 'column', md: 'row' } }}>
+            <Box sx={{ ml: { xs: 0, sm: 5 }, display: 'flex', flexDirection: { xs: 'row', sm: 'row', md: 'column' }, justifyContent: 'space-evenly' }}>
               <Typography variant="h4" sx={{ mt: .5 }}>ABOUT</Typography>
               <Typography variant="h4" sx={{ mt: .5 }}>PORTFOLIO</Typography>
               <Typography variant="h4" sx={{ mt: .5 }}>CONTACT</Typography>
             </Box>
-            <Box sx={{ mr: { xs: 0, sm: 5 }, mt: 2, mb: 2, alignSelf: { xs: 'center' } }}>
+            <Box sx={{ mt: {xs: 0, sm: 0, md: 2}, mb: 2, alignSelf: { xs: 'center' } }}>
               <Typography variant="h2">
-                Bernie McKnight
+                BERNIE McKNIGHT
               </Typography>
-              <Typography variant="h3" sx={{ ml: 1 }}>
-                website developer
+              <Typography variant="h3">
+                WEBSITE DEVELOPER
               </Typography>
             </Box>
         </Box>
--- src/themes.js
+++ src/themes.js
@@ -1,47 +1,78 @@
 import { createTheme } from '@mui/material/styles';
 
 const raleway =  "'Raleway', sans-serif";
-const merriweather = "'Merriweather', serif";
+const gaegu = "'Gaegu', serif";
 
 const theme = createTheme();
 
+theme.palette.primary = {
+      light: '#DD99BB',
+      main: '#FFFFFF',
+      dark: '#6D466B',
+      contrastText: '#241909',
+    
+} 
+
 theme.typography.h2 = {
-  fontFamily: raleway,
+  color: '#F9C80E',
+  fontFamily: gaegu,
   fontWeight: 800,
-  fontSize: '3rem',
-  textAlign: 'left',
-  // '@media (min-width:300px)': {
-  //   fontSize: '1.5rem',
-  // },
-  [theme.breakpoints.down('sm')]: {
-    fontSize: '1rem',
+  textAlign: 'center',
+  [theme.breakpoints.up('xs')]: {
+    fontSize: '2.3rem',
+  },
+  [theme.breakpoints.up('sm')]: {
+    fontSize: '3rem',
+    color: 'green',
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '5.2rem',
+    color: 'blue',
   },
 }
 
 theme.typography.h3 = {
-  fontFamily: merriweather,
-  fontWeight: 300,
-  fontStyle: 'italic',
-  fontSize: '2rem',
-  textAlign: 'left',
-  '@media (min-width:300px)': {
+  color: '#A50104',
+  fontFamily: raleway,
+  fontWeight: 400,
+  // letterSpacing: '.75rem',
+  textAlign: 'center',
+    [theme.breakpoints.up('xs')]: {
     fontSize: '1rem',
+    letterSpacing: '.4rem',
+  },
+  [theme.breakpoints.up('sm')]: {
+    fontSize: '3rem',
+    color: 'green',
   },
   [theme.breakpoints.up('md')]: {
-    fontSize: '2.5rem',
+    fontWeight: 400,
+    fontSize: '2rem',
+    letterSpacing: '1.1rem',
   },
+  //     '@media (max-width:380px)': {
+  //   fontSize: '.25rem',
+  //   letterSpacing: '.5rem',
+  // },
+  // '@media (max-width:450px)': {
+  //   fontSize: '1rem',
+    
+  // },
 }
 
 theme.typography.h4 = {
+  color: '#6D466B',
   fontFamily: raleway,
-  fontWeight: 300,
+  fontWeight: 800,
   fontSize: '1rem',
   textAlign: 'left',
   // '@media (min-width:300px)': {
   //   fontSize: '.75rem',
   // },
   [theme.breakpoints.down('md')]: {
-    fontSize: '1.5rem',
+    fontWeight: 800,
+  fontSize: '1rem',
+  letterSpacing: '.1rem',
   },
 }
 
