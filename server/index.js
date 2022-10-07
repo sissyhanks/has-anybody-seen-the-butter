fd818118baa48b8d3b688eea61142aa81fa4d586
Bernadette McKnight <sissyhanks@yahoo.com>
2022-10-07 09:52:06 -0400

    set up MYsql DB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+require("dotenv").config();
+
 const path = require("path");
 const express = require("express");
+const Sequelize = require("sequelize");
+
+const sequelize = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const db = require("./config/connection");
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
@@ -18,6 +25,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
 });
